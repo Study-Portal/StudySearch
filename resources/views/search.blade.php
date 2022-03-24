82390520405a2e22ab8b8f12ce4229ebf27f3676
Will <twitchifygamers@gmail.com>
2022-03-24 20:52:12 +0000

    Added search button and input to search page

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -10,6 +10,14 @@
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
                     Search for anything here!
+                    <form>
+                        <div class="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
+                            <div class="relative">
+                                <input type="text" class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." role="combobox" aria-expanded="false" aria-controls="options">
+                            </div>
+                        </div>
+                        <x-button>Search</x-button>
+                    </form>
                 </div>
             </div>
         </div>
