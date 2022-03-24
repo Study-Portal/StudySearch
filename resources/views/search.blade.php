cc9b6f7e10688c9692c91d74919ac563b2de2f25
Will <twitchifygamers@gmail.com>
2022-03-24 21:05:53 +0000

    Added subject field to search as filter

--- resources/views/search.blade.php
+++ resources/views/search.blade.php
@@ -11,11 +11,26 @@
                 <div class="p-6 bg-white border-b border-gray-200">
                     Search for anything here!
                     <form>
-                        <div class="mx-auto max-w-xl transform divide-y divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
-                            <div class="relative">
-                                <input type="text" class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." role="combobox" aria-expanded="false" aria-controls="options">
+                        <div class="pb-3">
+                            <div>
+                                <div class="pb-3">
+                                    <div>
+                                        <label for="location" class="block text-sm font-medium text-gray-700">Subject</label>
+                                        <select id="location" name="location" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                                            <option>United States</option>
+                                            <option selected>Canada</option>
+                                            <option>Mexico</option>
+                                        </select>
+                                    </div>
+                                </div>
+                                <div class="max-w-fit transform  divide-gray-100 overflow-hidden rounded-xl bg-white shadow-2xl ring-1 ring-black ring-opacity-5 transition-all">
+                                    <div class="relative">
+                                        <input type="text" class="h-12 w-full border-0 bg-transparent pl-11 pr-4 text-gray-800 placeholder-gray-400 focus:ring-0 sm:text-sm" placeholder="Search..." role="combobox" aria-expanded="false" aria-controls="options">
+                                    </div>
+                                </div>
                             </div>
                         </div>
+
                         <x-button>Search</x-button>
                     </form>
                 </div>
