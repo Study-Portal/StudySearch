c392295979a53e2e054f347b9a38a6897f057f31
Will <twitchifygamers@gmail.com>
2022-04-29 18:50:33 +0100

    Added please verify your email message to create page

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -9,58 +9,65 @@
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
             <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
                 <div class="p-6 bg-white border-b border-gray-200">
-                    <form class="flex h-full w-full flex-col overflow-y-scroll bg-white" method="post" action="{{route('create.save')}}">
-                        <div class="flex-1">
-                            <!-- Divider container -->
-                            <div class="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
-                                <!-- Project name -->
-                                <div class="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
-                                    <div>
-                                        <label for="project-name" class="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"> Document title </label>
-                                    </div>
-                                    <div class="sm:col-span-2">
-                                        <input type="text" name="project-name" id="project-name" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
-                                    </div>
-                                </div>
 
-                                <!-- Project description -->
-                                <div class="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
-                                    <div>
-                                        <label for="project-description" class="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"> Description </label>
+                    @if (auth()->user()->hasVerifiedEmail())
+                        <form class="flex h-full w-full flex-col overflow-y-scroll bg-white" method="post" action="{{route('create.save')}}">
+                            <div class="flex-1">
+                                <!-- Divider container -->
+                                <div class="space-y-6 py-6 sm:space-y-0 sm:divide-y sm:divide-gray-200 sm:py-0">
+                                    <!-- Project name -->
+                                    <div class="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                                        <div>
+                                            <label for="project-name" class="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"> Document title </label>
+                                        </div>
+                                        <div class="sm:col-span-2">
+                                            <input type="text" name="project-name" id="project-name" class="block w-full rounded-md border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm">
+                                        </div>
                                     </div>
-                                    <div class="sm:col-span-2">
-                                        <textarea id="project-description" name="project-description" rows="3" class="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></textarea>
+
+                                    <!-- Project description -->
+                                    <div class="space-y-1 px-4 sm:grid sm:grid-cols-3 sm:gap-4 sm:space-y-0 sm:px-6 sm:py-5">
+                                        <div>
+                                            <label for="project-description" class="block text-sm font-medium text-gray-900 sm:mt-px sm:pt-2"> Description </label>
+                                        </div>
+                                        <div class="sm:col-span-2">
+                                            <textarea id="project-description" name="project-description" rows="3" class="block w-full rounded-md border border-gray-300 shadow-sm focus:border-indigo-500 focus:ring-indigo-500 sm:text-sm"></textarea>
+                                        </div>
                                     </div>
-                                </div>
 
-                                <div class="px-6">
-                                    <label for="subject" class="block text-sm font-medium text-gray-700">Subject</label>
-                                    <select id="subject" name="subject" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                                        @foreach($subjects as $s)
-                                            <option>{{$s->subject}}</option>
-                                        @endforeach
-                                    </select>
-                                </div>
+                                    <div class="px-6">
+                                        <label for="subject" class="block text-sm font-medium text-gray-700">Subject</label>
+                                        <select id="subject" name="subject" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                                            @foreach($subjects as $s)
+                                                <option>{{$s->subject}}</option>
+                                            @endforeach
+                                        </select>
+                                    </div>
 
-                                <div class="px-6">
-                                    <label for="privacy" class="block text-sm font-medium text-gray-700">Privacy</label>
-                                    <select id="privacy" name="privacy" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
-                                        <option selected value="0">Public</option>
-                                        <option value="1">Private</option>
-                                    </select>
-                                </div>
+                                    <div class="px-6">
+                                        <label for="privacy" class="block text-sm font-medium text-gray-700">Privacy</label>
+                                        <select id="privacy" name="privacy" class="mt-1 block w-full pl-3 pr-10 py-2 text-base border-gray-300 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm rounded-md">
+                                            <option selected value="0">Public</option>
+                                            <option value="1">Private</option>
+                                        </select>
+                                    </div>
 
+                                </div>
                             </div>
-                        </div>
 
-                        <!-- Action buttons -->
-                        <div class="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
-                            <div class="flex justify-end space-x-3">
-                                <button type="button" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Cancel</button>
-                                <button type="submit" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Submit</button>
+                            <!-- Action buttons -->
+                            <div class="flex-shrink-0 border-t border-gray-200 px-4 py-5 sm:px-6">
+                                <div class="flex justify-end space-x-3">
+                                    <button type="button" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Cancel</button>
+                                    <button type="submit" class="rounded-md border border-gray-300 bg-white py-2 px-4 text-sm font-medium text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2">Submit</button>
+                                </div>
                             </div>
-                        </div>
-                    </form>
+                        </form>
+                    @else
+                        <p>You must verify your email before you can post anything on Study Search.</p>
+                        <br>
+                        <p>If you believe this is a mistake then please contact support.</p>
+                    @endif
 
                 </div>
             </div>
