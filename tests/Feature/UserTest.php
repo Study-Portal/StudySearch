dab0f9956cede33ba7fc04fc70643ddb97826af3
Will <twitchifygamers@gmail.com>
2022-05-15 17:43:06 +0100

    Created more epic tests with extra effeciency

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -9,13 +9,29 @@ use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-    public function test_user_redirects_from_home_page()
+    public function user()
     {
         $user = User::factory()->create();
         $this->assertModelExists($user);
         $this->actingAs($user);
         $this->assertAuthenticated();
+
+        return $user->name;
+    }
+
+    public function test_user_redirects_from_home_page()
+    {
+        $this->user();
         $view = $this->get(route('home'));
         $view->assertRedirect(route('dashboard'));
+    }
+
+    public function test_user_can_view_dashboard()
+    {
+        $u = $this->user();
+        $view = $this->get(route('dashboard'));
+        $view->assertOk();
+        $view->assertViewIs('dashboard');
+        $view->assertSeeText($u);
     }
 }
