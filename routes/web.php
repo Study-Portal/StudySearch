b3d6a3d4553a93e7ac4261e55b0b723a02665a07
Will <twitchifygamers@gmail.com>
2022-05-15 17:36:47 +0100

    Created test that requires db

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->middleware('guest');
+})->middleware('guest')->name('home');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,11 +2,20 @@
 
 namespace Tests\Feature;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 class UserTest extends TestCase
 {
-
+    public function test_user_redirects_from_home_page()
+    {
+        $user = User::factory()->create();
+        $this->assertModelExists($user);
+        $this->actingAs($user);
+        $this->assertAuthenticated();
+        $view = $this->get(route('home'));
+        $view->assertRedirect(route('dashboard'));
+    }
 }
